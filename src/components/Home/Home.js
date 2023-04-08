8e3b0ff9b54ff0a54a65bfbe2be5f7c4ae21dee9
Sintiya Hristova <hristova.sintiya@gmail.com>
2023-04-08 15:38:24 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import video from "../../Resoures/fairityle.mp4";
 import { useAuth } from "../../Context/AuthContext";
 
 const Home = () => {
-  const { currentUser } = useAuth();
+  useAuth();
   return (
     <div className="home-page">
       <video type="vide/mp4" src={video} autoPlay={true} loop={true}></video>
