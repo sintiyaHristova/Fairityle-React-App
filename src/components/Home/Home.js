4a43e2cd1fd2f1f69c3e2188be7f2b1df99b2f5e
Sintiya Hristova <hristova.sintiya@gmail.com>
2023-04-08 22:11:58 +0300

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,5 @@
 import "./Home.css";
 import video from "../../Resoures/fairityle.mp4";
-import { Link } from "react-router-dom";
 import { useAuth } from "../../Context/AuthContext";
 
 const Home = () => {
@@ -9,11 +8,6 @@ const Home = () => {
     <div className="home-page">
       <video type="vide/mp4" src={video} autoPlay={true} loop={true}></video>
       <p className="info-message">Разкажи ми приказка...</p>
-      <p className="login-message">
-        <Link to="/all" className="login-btn">
-          КАТАЛОГ
-        </Link>
-      </p>
     </div>
   );
 };
